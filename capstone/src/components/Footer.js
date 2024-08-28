3d7b0466adb2e014bab261120602d039383b7b38
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-08-28 12:36:25 +0200

    Add meta tags for OG protocol

--- capstone/src/components/Footer.js
+++ capstone/src/components/Footer.js
@@ -1,5 +1,28 @@
 export default function Footer() {
     return (
-        <footer/>
+        <footer>
+            <ul>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#menu'>Menu</a></li>
+                <li><a href='#reservations'>Reservations</a></li>
+                <li><a href='#order'>Order Online</a></li>
+                <li><a href='#login'>Log In</a></li>
+            </ul>
+
+            <h3>Contact</h3>
+            <ul>
+                <li>Address: blank</li>
+                <li>Phone: blank</li>
+                <li>Email: blank</li>
+            </ul>
+
+            <h3>Social media</h3>
+            <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Tik Tok</li>
+            </ul>
+        </footer>
     )
 }
