83c5e95bcb9fc178ecfe2725c3813317b4a4493d
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-08-28 14:06:11 +0200

    Move logo from header to nav

--- capstone/src/components/Header.js
+++ capstone/src/components/Header.js
@@ -1,7 +1,6 @@
 export default function Header(){
     return(
         <header>
-            <img src='../../assets/Logo.svg' alt='Little Lemon Logo'></img>
         </header>
     )
 }
--- capstone/src/components/Nav.js
+++ capstone/src/components/Nav.js
@@ -2,6 +2,7 @@ export default function Nav(){
     return(
         <nav>
             <ul>
+                <li><img src='../../assets/Logo.svg' alt='Little Lemon Logo'></img></li>
                 <li><a href='#home'>Home</a></li>
                 <li><a href='#about'>About</a></li>
                 <li><a href='#menu'>Menu</a></li>
