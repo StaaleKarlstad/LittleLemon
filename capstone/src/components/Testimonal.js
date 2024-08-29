06ef4a8a63647d9dea32f47c6aac5a569a7eb290
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-08-29 12:07:10 +0200

    Create testimonal component

---
+++ capstone/src/components/Testimonal.js
@@ -0,0 +1,12 @@
+export default function Testimonial() {
+    return (
+        <article>
+            <div className='testimonal-rating'>
+                Rating
+            </div>
+            <img src='' alt='user image' clasName='testimonial-image'></img>
+            <div className='testimonal-user'></div>
+            <div className='testimonal-text'></div>
+        </article>
+    )
+}
