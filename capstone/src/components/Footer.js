91080d84f4449d76d9daf6a797d10034516417f2
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-08-29 15:18:45 +0200

    Add logo to footer

--- capstone/src/components/Footer.js
+++ capstone/src/components/Footer.js
@@ -1,28 +1,35 @@
 export default function Footer() {
     return (
         <footer>
-            <ul>
-                <li><a href='#home'>Home</a></li>
-                <li><a href='#about'>About</a></li>
-                <li><a href='#menu'>Menu</a></li>
-                <li><a href='#reservations'>Reservations</a></li>
-                <li><a href='#order'>Order Online</a></li>
-                <li><a href='#login'>Log In</a></li>
-            </ul>
+            <img src='../../assets/logo.svg' alt='little lemon logo' className='footer-logo'></img>
+            <div className='footer-navigate'>
+                <h3>Navigate</h3>
+                <ul>
+                    <li><a href='#home'>Home</a></li>
+                    <li><a href='#about'>About</a></li>
+                    <li><a href='#menu'>Menu</a></li>
+                    <li><a href='#reservations'>Reservations</a></li>
+                    <li><a href='#order'>Order Online</a></li>
+                    <li><a href='#login'>Log In</a></li>
+                </ul>
+            </div>
 
-            <h3>Contact</h3>
-            <ul>
-                <li>Address: blank</li>
-                <li>Phone: blank</li>
-                <li>Email: blank</li>
-            </ul>
-
-            <h3>Social media</h3>
-            <ul>
-                <li>Facebook</li>
-                <li>Instagram</li>
-                <li>Tik Tok</li>
-            </ul>
+            <div className='footer-contact'>
+                <h3>Contact</h3>
+                <ul>
+                    <li>Address: blank</li>
+                    <li>Phone: blank</li>
+                    <li>Email: blank</li>
+                </ul>
+            </div>
+            <div className='footer-social'>
+                <h3>Social media</h3>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>Tik Tok</li>
+                </ul>
+            </div>
         </footer>
     )
 }
