d1e8fc14d03a805cf60eab494cff35a60e32b401
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-08-29 15:18:14 +0200

    Add 'About' section

---
+++ capstone/src/components/About.js
@@ -0,0 +1,14 @@
+export default function About(){
+    return (
+        <section>
+            <h1>Little lemon</h1>
+            <h3>Chicago</h3>
+            <p>
+               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+            </p>
+            <img src='../../assets/restaurant_chef_B.jpg' alt='chef' className='about-image1' style={{width: '100px', height: '100px'}}></img>
+            <img src='../../assets/restaurant.jpg' alt='restaurant' className='about-image2' style={{width: '100px', height: '100px'}}></img>
+        </section>
+    )
+}
--- capstone/src/components/Main.js
+++ capstone/src/components/Main.js
@@ -1,11 +1,15 @@
 import Hero from './Hero.js'
 import Meals from './Meals.js'
+import Testimonal from './Testimonal.js'
+import About from './About.js'
 
 export default function Main(){
     return(
         <main>
             <Hero></Hero>
             <Meals></Meals>
+            <Testimonal></Testimonal>
+            <About></About>
         </main>
     )
 }
