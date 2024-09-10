11175c5ac130a311aa402bd9888dc85d6e149fec
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-10 15:14:22 +0200

    Add extra div containers

--- capstone/src/components/MealCard.js
+++ capstone/src/components/MealCard.js
@@ -1,11 +1,17 @@
 export default function MealCard(props){
     return(
-        <article>
-            <img src={props.imageURL} alt='meal-image' style={{width: '100px', height: '100px' }}></img>
-            <h3>{props.name}</h3>
-            <h5>{props.price}</h5>
-            <p>{props.info}</p>
-            <h4>Order a delivery</h4>
+        <article className='mealcard-container'>
+            <div className='mealcard-image'>
+                <img src={props.imageURL} alt='meal-image'></img>
+            </div>
+            <div className='mealcard-text'>
+                <div className='mealcard-text-headline'>
+                    <h4>{props.name}</h4>
+                    <h5 className='mealcard-price'>{props.price}</h5>
+                </div>
+                <p>{props.info}</p>
+                <h4>Order a delivery</h4>
+            </div>
         </article>
     )
 }
--- capstone/src/components/Meals.js
+++ capstone/src/components/Meals.js
@@ -3,29 +3,31 @@ import Button from './Button.js'
 
 export default function Meals(){
     return(
-        <section className='meals' style={{backgroundColor: 'red'}}>
-            <div className='headline'>
+        <section className='meals'>
+            <div className='meals-headline'>
                 <h1>This weeks specials!</h1>
                 <Button text='Online Menu'></Button>
             </div>
-            <MealCard
-                imageURL='../../assets/greek_salad.jpg'
-                name='Greek Salad'
-                price='12.99'
-                info='Delicious and nutricious! Green stuff drenched in fluids'>
-            </MealCard>
-            <MealCard
-                imageURL='../../assets/greek_salad.jpg'
-                name='Greek Salad'
-                price='12.99'
-                info='Delicious and nutricious! Green stuff drenched in fluids'>
-            </MealCard>
-            <MealCard
-                imageURL='../../assets/greek_salad.jpg'
-                name='Greek Salad'
-                price='12.99'
-                info='Delicious and nutricious! Green stuff drenched in fluids'>
-            </MealCard>
+            <div className='meals-cards'>
+                <MealCard
+                    imageURL='../../assets/greek_salad.jpg'
+                    name='Greek Salad'
+                    price='$ 12.99'
+                    info='Delicious and nutricious! Green stuff drenched in fluids'>
+                </MealCard>
+                <MealCard
+                    imageURL='../../assets/greek_salad.jpg'
+                    name='Greek Salad'
+                    price='$ 12.99'
+                    info='Delicious and nutricious! Green stuff drenched in fluids'>
+                </MealCard>
+                <MealCard
+                    imageURL='../../assets/greek_salad.jpg'
+                    name='Greek Salad'
+                    price='$ 12.99'
+                    info='Delicious and nutricious! Green stuff drenched in fluids'>
+                </MealCard>
+            </div>
         </section>
     )
 }
