6236cc188bbef5e857ca0e8639b4eb220cf734d5
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-12 18:26:38 +0200

    Switch from a tags to Link in navbar

--- capstone/src/components/Nav.js
+++ capstone/src/components/Nav.js
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom"
+
 export default function Nav(){
     return(
         <nav>
             <ul>
                 <li><img src='../../assets/Logo.svg' alt='Little Lemon Logo'></img></li>
-                <li><a href='#home'>Home</a></li>
-                <li><a href='#about'>About</a></li>
-                <li><a href='#menu'>Menu</a></li>
-                <li><a href='#reservations'>Reservations</a></li>
-                <li><a href='#order'>Order Online</a></li>
-                <li><a href='#login'>Log In</a></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/meals'>Menu</Link></li>
+                <li><Link to='/reservations'>Reservations</Link></li>
+                <li><Link to='/testimonial'>Reviews</Link></li>
+                <li><Link to='/login'>Log In</Link></li>
             </ul>
         </nav>
     )
