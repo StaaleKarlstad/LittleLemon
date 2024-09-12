a838b6c1f2b36d0bcd6157d65a3f3b63fa5fcac9
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-12 18:26:18 +0200

    Add routing in Main.js

--- capstone/src/components/Main.js
+++ capstone/src/components/Main.js
@@ -2,14 +2,42 @@ import Hero from './Hero.js'
 import Meals from './Meals.js'
 import Testimonal from './Testimonal.js'
 import About from './About.js'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Wrapper from './Wrapper.js';
+import Booking from './Booking.js';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Wrapper>
+                    <Hero/>
+                    <Meals/>
+                    <Testimonal/>
+                    <About/>
+                </Wrapper>
+    },
+    {
+        path: '/testimonial',
+        element: <Wrapper><Testimonal></Testimonal></Wrapper>
+    },
+    {
+        path: '/meals',
+        element: <Wrapper><Meals></Meals></Wrapper>
+    },
+    {
+        path: '/about',
+        element: <Wrapper><About></About></Wrapper>
+    },
+    {
+        path: '/booking',
+        element: <Wrapper><Booking></Booking></Wrapper>
+    }
+])
 
 export default function Main(){
     return(
         <main>
-            <Hero></Hero>
-            <Meals></Meals>
-            <Testimonal></Testimonal>
-            <About></About>
+            <RouterProvider router={router} ></RouterProvider>
         </main>
     )
 }
