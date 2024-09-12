9bcd38fdf2bfb088a0c8bd59ebafeb00d6f08492
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-12 18:27:04 +0200

    Create Booking and BookingForm component

---
+++ capstone/src/components/Booking.js
@@ -0,0 +1,10 @@
+import BookingForm from "./BookingForm";
+
+export default function Booking() {
+    return(
+        <>
+            <h1>Book a table</h1>
+            <BookingForm></BookingForm>
+        </>
+    )
+}
