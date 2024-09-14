c6c601a2656338ce859f0707f0834a678eac5b14
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-14 19:51:49 +0200

    Add submit function as props

--- capstone/src/components/Booking.js
+++ capstone/src/components/Booking.js
@@ -1,10 +1,11 @@
 import BookingForm from "./BookingForm";
+import AvailableTimes from "./AvailableTimes";
 
-export default function Booking() {
+export default function Booking({ availableTimes, dispatch, submit }) {
     return(
         <>
             <h1>Book a table</h1>
-            <BookingForm></BookingForm>
+            <BookingForm availableTimes={availableTimes} dispatch={dispatch} submit={submit}></BookingForm>
         </>
     )
 }
