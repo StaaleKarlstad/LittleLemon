f25cc159a0ae9acbc62b7d49bef9ab75dc7c85ee
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-14 19:50:27 +0200

    Implement event handlers and dynamic updating 'time' options

--- capstone/src/components/BookingForm.js
+++ capstone/src/components/BookingForm.js
@@ -1,29 +1,103 @@
-export default function BookingForm(){
-    return(
-            <form className='bookingform'>
-                <label htmlFor="firstname">Firstname</label>
-                <input type='text' id='firstname'></input>
-                <label htmlFor="lastname">Lastname</label>
-                <input type='text' id='lastname'></input>
-                <label htmlFor="res-date">Choose date</label>
-                <input type="date" id="res-date"></input>
-                <label htmlFor="res-time">Choose time</label>
-                <select id="res-time ">
-                    <option>17:00</option>
-                    <option>18:00</option>
-                    <option>19:00</option>
-                    <option>20:00</option>
-                    <option>21:00</option>
-                    <option>22:00</option>
-                </select>
-                <label htmlFor="guests">Number of guests</label>
-                <input type="number" placeholder="1" min="1" max="10" id="guests"></input>
-                <label htmlFor="occasion">Occasion</label>
-                <select id="occasion">
-                    <option>Birthday</option>
-                    <option>Anniversary</option>
-                </select>
-                <input type="submit" value="Make Your reservation"></input>
-            </form>
-    )
+import { useState } from 'react';
+
+
+export default function BookingForm( {availableTimes, dispatch, submit} ) {
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        date: '',
+        time: '',
+        guests: 1,
+        occasion: 'Birthday',
+        });
+
+
+    const handleInputChange = (e) => {
+        e.preventDefault();
+        const { id, value } = e.target;
+        console.log(value)
+        setFormData({ ...formData, [id]: value });
+      };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        submit(formData)
+      };
+
+    const handleDateChange = (e) => {
+        e.preventDefault();
+        const { id, value } = e.target;
+        const date = new Date(value)
+        dispatch(date);
+        handleInputChange(e);
+    }
+
+  return (
+    <form className='bookingform' onSubmit={handleSubmit}>
+      <label htmlFor="firstname">Firstname</label>
+      <input
+        type='text'
+        id='firstname'
+        data-testid='firstname'
+        value={formData.firstname}
+        onChange={handleInputChange}
+      />
+
+      <label htmlFor="lastname">Lastname</label>
+      <input
+        type='text'
+        id='lastname'
+        data-testid='lastname'
+        value={formData.lastname}
+        onChange={handleInputChange}
+      />
+
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="date"
+        data-testid='date'
+        value={formData.date}
+        onChange={handleDateChange}
+      />
+
+      <label htmlFor="res-time">Choose time</label>
+      <select
+          id="time"
+          data-testid='time'
+          value={formData.time}
+          onChange={handleInputChange}
+        >
+        {availableTimes.map(times => <option key={times}>{times}</option>)}
+      </select>
+
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        id="guests"
+        data-testid='guests'
+        min="1"
+        max="10"
+        value={formData.guests}
+        onChange={handleInputChange}
+      />
+
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        data-testid='occasion'
+        value={formData.occasion}
+        onChange={handleInputChange}
+      >
+        <option>Birthday</option>
+        <option>Anniversary</option>
+      </select>
+
+      <input 
+        data-testid='submit-button'
+        type="submit" 
+        value="Make Your reservation" 
+        onClick={handleSubmit}/>
+    </form>
+  );
 }
