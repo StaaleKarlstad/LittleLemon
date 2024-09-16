61c54a91cd326fb477990db7f8e8e1eb6e0fc22e
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-16 14:38:40 +0200

    Rename components

--- capstone/src/components/Booking.js
+++ capstone/src/components/Booking.js
@@ -1,11 +1,56 @@
-import BookingForm from "./BookingForm";
-import AvailableTimes from "./AvailableTimes";
+import { useReducer } from "react"
+import { useNavigate } from 'react-router-dom'
+import BookingForm from "./BookingForm"
 
-export default function Booking({ availableTimes, dispatch, submit }) {
-    return(
-        <>
-            <h1>Book a table</h1>
-            <BookingForm availableTimes={availableTimes} dispatch={dispatch} submit={submit}></BookingForm>
-        </>
+const seededRandom = function (seed) {
+    var m = 2**35 - 31;
+    var a = 185852;
+    var s = seed % m;
+    return function () {
+        return (s = s * a % m) / m;
+    };
+}
+
+const fetchAPI = function(date) {
+    let result = [];
+    let random = seededRandom(date.getDate());
+
+    for(let i = 17; i <= 23; i++) {
+        if(random() < 0.5) {
+            result.push(i + ':00');
+        }
+        if(random() < 0.5) {
+            result.push(i + ':30');
+        }
+    }
+    return result;
+};
+
+const submitAPI = function(formData) {
+    return true;
+};
+
+export const updateTimes = (state, date) => {
+    return fetchAPI(date)
+}
+
+export const initializeTimes = () => {
+    return fetchAPI(new Date());
+}
+
+export default function Booking(){
+    const initial = initializeTimes();
+    const [availableTimes, dispatch] = useReducer(updateTimes, initial)
+    const navigate = useNavigate();
+
+    const succesfulSubmit = (bookingInfo) => {
+        const submit = submitAPI(bookingInfo)
+        if (submit){
+            navigate('/submit')
+        }
+    }
+
+    return (
+        <BookingForm availableTimes={availableTimes} dispatch={dispatch} submit={succesfulSubmit}></BookingForm>
     )
 }
