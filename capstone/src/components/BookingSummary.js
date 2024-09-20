e9cd7c7b285cf7f7e0a36259ed37c0bc659cec0f
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-20 11:23:56 +0200

    Style booking summary

--- capstone/src/components/BookingSummary.js
+++ capstone/src/components/BookingSummary.js
@@ -1,26 +1,50 @@
-
-
-export default function BookingSummary({formData, handleGoBack, handleSubmit}){
-    return (
-        <div className='booking-summary'>
-                <h1>BookingInfo</h1>
-                <p>Firstname: {formData.firstname}</p>
-                <p>Lastname: {formData.lastname}</p>
-                <p>Date: {formData.date}</p>
-                <p>Time: {formData.time}</p>
-                <p>Guests: {formData.guests}</p>
-                <p>Occasion: {formData.occasion}</p>
-            <button
-              onClick={handleGoBack}>
-                Back
-            </button>
-            <button
-                data-testid='submit-button'
-                type="submit"
-                value="Confirm Reservation"
-                onClick={handleSubmit}>
-                    Confirm reservation
-                </button>
+export default function BookingSummary({
+  formData,
+  handleGoBack,
+  handleSubmit,
+}) {
+  return (
+    <div className="booking-summary-container">
+      <h1>Your Reservation</h1>
+      <div className="booking-summary">
+        <div className="booking-summary-info">
+          <h3>Firstname:</h3>
+          <p>{formData.firstname}</p>
         </div>
-    )
+        <div className="booking-summary-info">
+          <h3>Lastname: </h3>
+          <p>{formData.lastname}</p>
+        </div>
+        <div className="booking-summary-info">
+          <h3>Date: </h3>
+          <p>{formData.date}</p>
+        </div>
+        <div className="booking-summary-info">
+          <h3>Time: </h3>
+          <p>{formData.time}</p>
+        </div>
+        <div className="booking-summary-info">
+          <h3>Guests: </h3>
+          <p> {formData.guests}</p>
+        </div>
+        <div className="booking-summary-buttons">
+          <button
+            className="booking-summary-button-back"
+            onClick={handleGoBack}
+          >
+            Back
+          </button>
+          <button
+            className="booking-summary-button-confirm"
+            data-testid="submit-button"
+            type="submit"
+            value="Confirm"
+            onClick={handleSubmit}
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
