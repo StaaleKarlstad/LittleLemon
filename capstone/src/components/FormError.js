1651ca963049f2408f1bc574b96abd7071eaab03
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-20 11:28:41 +0200

    Add test id to FormError

--- capstone/src/components/FormError.js
+++ capstone/src/components/FormError.js
@@ -1,6 +1,6 @@
 export default function FormError(props){
     return(
-        <div className="form-error">
+        <div className="form-error" data-testid="form-error">
             <span className="form-error-message">{props.message}</span>
         </div>
     )
