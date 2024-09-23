3e741def8d922bdc37732a35466404e6f8f3d35a
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-09-23 13:48:28 +0200

    Make Nav a child component of Header

--- capstone/src/components/Header.js
+++ capstone/src/components/Header.js
@@ -1,6 +1,17 @@
-export default function Header(){
-    return(
-        <header>
-        </header>
-    )
+import Nav from "./Nav";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+    const navigate = useNavigate()
+    const handleLogoClick = () => {
+        navigate('/')
+    }
+  return (
+    <header>
+      <img className="header-logo" src="../../assets/Logo.svg" alt="Little Lemon Logo" onClick={handleLogoClick}>
+      </img>
+      <Nav></Nav>
+    </header>
+  );
 }
